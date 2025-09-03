c8a0521784ca3033bf17bf4abf20bfb2c5ebc5fc
George <zhora.sviridov.05@bk.ru>
2025-09-03 23:45:27 +0500

    Начата работа с фронтом

--- src/SeaFight.Domain/obj/Debug/net9.0/SeaFight.Domain.AssemblyInfo.cs
+++ src/SeaFight.Domain/obj/Debug/net9.0/SeaFight.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeaFight.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+347caecaa01ec6528439895c42545605cf621503")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a77135ad9ccd39696278ba4f981a0b7748e224")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeaFight.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeaFight.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
